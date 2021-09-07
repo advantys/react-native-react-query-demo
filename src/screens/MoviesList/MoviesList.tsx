3324fbef42226561c835028aa821cc9d767d8313
arnaudbzn <arnaud.bezancon@advantys.com>
2021-09-07 08:45:08 +0200

    feat: simple list

--- src/screens/MoviesList/MoviesList.tsx
+++ src/screens/MoviesList/MoviesList.tsx
@@ -3,7 +3,7 @@ import { View, StyleSheet, FlatList, RefreshControl } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useTheme } from 'react-native-paper';
 
-import { useInfiniteMovies } from '@app/screens/hooks/useInfiniteMovies';
+import { useMovies } from '@app/screens/hooks/useMovies';
 import { Divider } from '@app/components/Divider';
 import { MainStack } from '@app/navigation/types';
 import { ListItem, LIST_ITEM_HEIGHT } from '@app/components/ListItem';
@@ -12,7 +12,6 @@ import { MovieFragment } from '@app/services/graphql';
 import { useOnlineStatus } from '@app/providers/hooks/useOnlineStatus';
 import { useRefreshOnFocus } from '@app/hooks/useRefreshOnFocus';
 import { useRefreshByUser } from '@app/hooks/useRefreshByUser';
-import { ListFooterComponent } from '@app/components/ListFooterComponent';
 
 type MoviesListScreenNavigationProp = StackNavigationProp<
   MainStack,
@@ -27,8 +26,7 @@ export function MoviesList({ navigation }: Props) {
   const theme = useTheme();
   const isOnline = useOnlineStatus();
 
-  const { movies, refetch, fetchNextPage, isFetchingNextPage } =
-    useInfiniteMovies();
+  const { movies, refetch } = useMovies();
 
   const { isRefetchingByUser, refetchByUser } = useRefreshByUser(refetch);
   useRefreshOnFocus(refetch);
@@ -42,13 +40,8 @@ export function MoviesList({ navigation }: Props) {
     [navigation]
   );
 
-  function onEndReached() {
-    console.log('onEndReached');
-    fetchNextPage();
-  }
-
   const renderItem = React.useCallback(
-    ({ item }: { item: MovieFragment | null }) => {
+    ({ item }: { item: MovieFragment }) => {
       return <ListItem item={item} onPress={onListItemPress} />;
     },
     [onListItemPress]
@@ -69,8 +62,6 @@ export function MoviesList({ navigation }: Props) {
         data={movies}
         keyExtractor={(item, index) => item?.id?.toString() || index.toString()}
         renderItem={renderItem}
-        onEndReached={onEndReached}
-        onEndReachedThreshold={1}
         style={[
           styles.fill,
           {
@@ -84,9 +75,6 @@ export function MoviesList({ navigation }: Props) {
             }}
           />
         )}
-        ListFooterComponent={
-          <ListFooterComponent isFetchingNextPage={isFetchingNextPage} />
-        }
         getItemLayout={(_data, index) => ({
           length: LIST_ITEM_HEIGHT,
           offset: LIST_ITEM_HEIGHT * index,

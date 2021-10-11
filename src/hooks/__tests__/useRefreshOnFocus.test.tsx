0a486c9ab4d563c172f35d33bf0c15194d0b17e4
arnaudbzn <arnaud.bezancon@advantys.com>
2021-10-11 12:08:16 +0200

    test: add useRefreshOnFocus

---
+++ src/hooks/__tests__/useRefreshOnFocus.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, act } from '@testing-library/react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { useRefreshOnFocus } from '@app/hooks/useRefreshOnFocus';
+
+describe('useRefreshOnFocus status hook tests', () => {
+  it('Should return call the refetch function on the second screen focus', async () => {
+    const spyRefetch = jest.fn();
+
+    const TestScreen = () => {
+      useRefreshOnFocus(spyRefetch);
+      return null;
+    };
+
+    const navigation = React.createRef<any>();
+    const Stack = createStackNavigator();
+
+    const Container = () => {
+      return (
+        <NavigationContainer ref={navigation}>
+          <Stack.Navigator>
+            <Stack.Screen name="screen1" component={TestScreen} />
+            <Stack.Screen name="screen2" component={() => null} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    };
+
+    render(<Container />);
+
+    // Should not refetch on the first screen1 focus
+    expect(spyRefetch).not.toBeCalled();
+
+    act(() => navigation.current.navigate('screen2'));
+
+    // Should refetch on the second screen1 focus
+    act(() => navigation.current.navigate('screen1'));
+    expect(spyRefetch).toBeCalled();
+  });
+});
